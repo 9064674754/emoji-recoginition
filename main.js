d3851282ed589f064e4be827056299a95fbac49d
9064674754 <98456102+9064674754@users.noreply.github.com>
2022-06-06 22:54:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+Webcam.set({
+  width:350,
+  height:300,
+  image_format:'png',
+  png_quality:100
+});
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+function take_snapshot(){
+  Webcam.snap(function(data_uri){
+      document.getElementById("result").innerHTML='<img id="captured_img" src="'+data_uri+'"/>';
+  })
+}
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/HMdn4lgyK/model.json',modelLoaded);
+function speak(){
+  var synth=window.speechSynthesis;
+  speak_data_1="Your gesture :" + gesture;
+  
+  var utterThis= new SpeechSynthesisUtterance(speak_data_1);
+  synth.speak(utterThis);
+}
+function modelLoaded(){
+  console.log("model Loaded");
+}
+function check()
+  {
+    
+    img = document.getElementById("captured_img");
+    classifier.classify(img, gotResult);
+  }
+
+
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    document.getElementById("potatopc").innerHTML = " ⠀ Prediction : "+results[0].label +" ⠀ ";
+   
+  }
+}
